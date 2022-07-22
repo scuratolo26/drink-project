39b70dfbe59022d1ba4227eb70f869c0f9e094e8
Brian Gee <briangeertsma@gmail.com>
2022-07-21 20:30:32 -0400

    added random fact query

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-const QUERY_URL = ("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=");
-const COCKTAIL_SEARCH = document.querySelector("cocktail-search");
+const DRINK_QUERY_URL = ("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=");
+const RANDOM_FACT_QUERY = ("https://uselessfacts.jsph.pl/random.json?language=en");
+const COCKTAIL_SEARCH = document.querySelector("#cocktail-search");
 
 COCKTAIL_SEARCH.addEventListener("keyup", (e) => {
     if (e.keyCode == 13) {
@@ -10,7 +11,7 @@ COCKTAIL_SEARCH.addEventListener("keyup", (e) => {
 
 
 function getDrink() {
-    fetch(QUERY_URL + COCKTAIL_SEARCH.value)
+    fetch(DRINK_QUERY_URL + COCKTAIL_SEARCH.value)
         .then(function (response) {
         return response.json();
         })
@@ -19,3 +20,13 @@ function getDrink() {
         })
 }
 
+function getRandomFact() {
+    fetch(RANDOM_FACT_QUERY)
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (data){
+        console.log(data.text);
+    })
+}
+getRandomFact();
