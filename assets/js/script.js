c77cc369ed7fc1841229c45f805e4490a6885f24
Brian Gee <briangeertsma@gmail.com>
2022-07-25 00:16:09 -0400

    functional cocktail list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 const DRINK_QUERY_URL = ("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=");
+const COCKTAIL_DETAIL_QUERY_URL = ("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=")
 const RANDOM_FACT_QUERY = ("https://uselessfacts.jsph.pl/random.json?language=en");
 const COCKTAIL_SEARCH = document.querySelector("#cocktail-search");
 const COCKTAIL_SEARCH_BTN = document.querySelector("#cocktail-search-btn");
+const COCKTAIL_LIST = document.querySelector(".cocktail-container")
 
 COCKTAIL_SEARCH_BTN.addEventListener("click", () => {
     getDrink();
@@ -14,16 +16,88 @@ COCKTAIL_SEARCH.addEventListener("keyup", (e) => {
 
 
 
+function getDrinkDetails(id) {
+    fetch(COCKTAIL_DETAIL_QUERY_URL + id)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            removeChildren(COCKTAIL_LIST);
+            var drinkName = document.createElement("h2");
+            var drinkPic = document.createElement("img");
+            var drinkGlass = document.createElement("p");
+            var drinkIngredient1 = document.createElement("p");
+            var drinkMeasure1 = document.createElement("p");
+            var drinkInstructions = document.createElement("p");
+            var addToFavoritesBtn = document.createElement("button");
+
+            drinkName.textContent = data.drinks[0].strDrink;
+            drinkPic.src = data.drinks[0].strDrinkThumb;
+            drinkGlass.textContent = data.drinks[0].strGlass;
+            drinkIngredient1.textContent = data.drinks[0].strIngredient1 + " " + data.drinks[0].strMeasure1;
+            drinkInstructions.textContent = data.drinks[0].strInstructions;
+            addToFavoritesBtn.textContent = ("Add drink to favorites");
+            addToFavoritesBtn.addEventListener("click", () => {
+                addDrinkToFavorites(); // Create this function
+            });
+
+
+            COCKTAIL_LIST.appendChild(drinkName);
+            COCKTAIL_LIST.appendChild(drinkPic);
+            COCKTAIL_LIST.appendChild(drinkGlass);
+            COCKTAIL_LIST.appendChild(drinkIngredient1);
+            COCKTAIL_LIST.appendChild(drinkMeasure1);
+            COCKTAIL_LIST.appendChild(drinkInstructions);
+            COCKTAIL_LIST.appendChild(addToFavoritesBtn);
+
+
+            // console.log(data.drinks[0].strDrink);
+
+            // for(var i=0; i<16; i++) {
+            //     str = "data.drinks[0].strMeasure" + i
+            //     if (str.length > 1) {
+            //         console.log(data.strMeasure[i]);
+
+            //     }
+            // }
+
+
+            console.log(data);
+        })
+}
+
 function getDrink() {
     fetch(DRINK_QUERY_URL + COCKTAIL_SEARCH.value)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
-            console.log(data.drinks[0].strDrink);
+            removeChildren(COCKTAIL_LIST);
+            displayDrinks(data.drinks);
         })
+        .catch(error => console.log('error', error));
 }
+
+function displayDrinks(list) {
+    list.forEach(e => {
+        var li = document.createElement("p");
+        li.textContent = e.strDrink;
+        li.addEventListener("click", function() {
+            getDrinkDetails(e.idDrink);
+        });
+        li.classList.add("cocktail-list-item")
+        COCKTAIL_LIST.appendChild(li);
+    });
+
+}
+
+// Removes all children from a parent element
+function removeChildren(parent){
+    while (parent.lastChild) {
+        parent.removeChild(parent.lastChild);
+    }
+}
+
 
 // define container variable - sam
 var factContainer = $("#random-fact-container");
